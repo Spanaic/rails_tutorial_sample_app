6fb41282f8fb6113463f02c9254a42c95eb1cab2
Spanaic <spanaic@gmail.com>
2020-03-30 12:33:19 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/new', to: "users#new", as: "signup"
-
   root "static_pages#home"
   # get 'static_pages/help'
   get '/help', to:'static_pages#help' # 名前付きpathを設定する書き方
@@ -8,4 +6,6 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   # get 'static_pages/contact'
   get 'contact', to: 'static_pages#contact'
+  post '/signup', to: 'users#create'
+  resources :users
 end
