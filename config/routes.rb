1cbc3b801c98e9f6ecc453812e5430716af3e161
Spanaic <spanaic@gmail.com>
2020-03-30 15:25:02 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   # get 'static_pages/help'
-  get '/help', to:'static_pages#help' # 名前付きpathを設定する書き方
+  get '/help', to: 'static_pages#help' # 名前付きpathを設定する書き方
   # get 'static_pages/about'
   get '/about', to: 'static_pages#about'
   # get 'static_pages/contact'
   get 'contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
