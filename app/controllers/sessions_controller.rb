2c4612a875a14c2746a6936c73624ca70957554f
Spanaic <spanaic@gmail.com>
2020-04-01 14:44:02 +0900

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,24 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in(user)
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user) # sessionにもcookiesにも記憶させないということ(forget)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user) # sessionにもcookiesにも記憶させないということ(forget)
+        redirect_back_or user
+      else
+        message = 'Account not activated.'
+        message += 'Check your email for the activation link.'
+        flash.now[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
-    render :new
+      render :new
     end
   end
 
   def destroy
     log_out if logged_in? # current_userが居るかどうか確認することで、別タブでログアウトされた場合に備える
-    redirect_to root_url  # 処理の前に真偽値による条件分岐を行うことで、エラーを出さずに処理が実行されるように備える
-  end                     # log_outの処理が行われないだけで、redirectは行われる if logged_in
+    redirect_to root_url # 処理の前に真偽値による条件分岐を行うことで、エラーを出さずに処理が実行されるように備える
+  end # log_outの処理が行われないだけで、redirectは行われる if logged_in
 end
